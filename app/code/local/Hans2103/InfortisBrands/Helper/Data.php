7b7f441b258935ca0250c294d998e825d6eb281c
Hans Kuijpers <hans2103@gmail.com>
2017-07-05 14:11:15 +0200

    init commit

---
+++ app/code/local/Hans2103/InfortisBrands/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+class Hans2103_Brands_Helper_Data extends Infortis_Brands_Helper_Data
+{
+    /**
+     * Get path of the directory with brand images
+     *
+     * @return string
+     */
+    public function getBrandImagePath()
+    {
+        return 'wysiwyg/brands/';
+    }
+}
